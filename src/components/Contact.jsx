3f89fac8e110093601d5dc7068837294419d43b7
Kunal <kunalpa239@gmail.com>
2022-05-04 10:54:02 +0530

    first commit

---
+++ src/components/Contact.jsx
@@ -0,0 +1,93 @@
+import React from "react";
+import { FaGithub, FaInstagram, FaLinkedinIn } from "react-icons/fa";
+import { HiOutlineMail } from "react-icons/hi";
+
+const Contact = () => {
+  return (
+    <>
+      <div
+        name="contact"
+        className="pt-[8rem]  w-full h-screen bg-[#0a192f] flex justify-center items-center p-4"
+      >
+        <form
+          method="POST"
+          action="https://getform.io/f/fbd49775-35d6-45b1-ad8d-e26907319824"
+          className="flex flex-col max-w-[600px] w-full"
+        >
+          <div className="pb-8">
+            <p className="text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300">
+              Contact
+            </p>
+            <p className="text-gray-300 py-4">
+              //Submit the form below or shoot me an email -
+              kunalpatil2391@gmail.com
+            </p>
+          </div>
+          <input
+            className="bg-[#ccd6f6] p-2"
+            type="text"
+            placeholder="Name"
+            name="name"
+          />
+          <input
+            className="my-4 p-2 bg-[#ccd6f6]"
+            type="email"
+            placeholder="Email"
+            name="email"
+          />
+          <textarea
+            className="bg-[#ccd6f6] p-2"
+            name="message"
+            placeholder="Message"
+            rows="10"
+          ></textarea>
+          <button className="text-white border-2 hover:bg-pink-600 hover:border-pink-600 px-4 py-3 my-8 mx-auto flex items-center">
+            Submit
+          </button>
+        </form>
+      </div>
+      {/* footer */}
+      <div className="footer w-full pt-[4rem] pb-[2.5rem] bg-[#0a192f] flex justify-center items-center text-gray-300">
+        <div className="flex flex-col justify-center items-center">
+          <p>Connect on Social Media</p>
+          <div className="flex justfy-center mt-4">
+            <div className="mr-3">
+              <a
+                href="#"
+                className="hover:text-red-600 transition ease-out duration-300"
+              >
+                <FaLinkedinIn size={25} />
+              </a>
+            </div>
+            <div className="mr-3">
+              <a
+                href="#"
+                className="hover:text-red-600 transition ease-out duration-300"
+              >
+                <FaGithub size={25} />
+              </a>
+            </div>
+            <div className="mr-3">
+              <a
+                href="#"
+                className="hover:text-red-600 transition ease-out duration-300"
+              >
+                <FaInstagram size={25} />
+              </a>
+            </div>
+            <div>
+              <a
+                href="#"
+                className="hover:text-red-600 transition ease-out duration-300"
+              >
+                <HiOutlineMail size={26} />
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Contact;
---
+++ src/components/Work.jsx
@@ -0,0 +1,221 @@
+import React from "react";
+import work from "../assets/workImg.jpeg";
+import note from "../assets/notezipper.PNG";
+import instafood2 from "../assets/instafood2.PNG";
+import instacart from "../assets/instacart.PNG";
+import instacart3 from "../assets/instacart3.PNG";
+import expense from "../assets/expensetracker.PNG";
+import todo from "../assets/todo.PNG";
+
+const Work = () => {
+  return (
+    <div
+      name="work"
+      className="pt-[5rem] sm:pt-0 w-full md:h-screen text-gray-300 bg-[#0a192f]"
+    >
+      <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600">
+            Work
+          </p>
+          <p className="py-6">Check out some of my recent works</p>
+        </div>
+
+        {/* container */}
+        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
+          {/* grid-item */}
+          <div
+            style={{ backgroundImage: `url(${note})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* hover effects */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                MERN Application
+              </span>
+              <div className="pt-8 text-center">
+                <a
+                  href="https://notezipper-mern-kunal.herokuapp.com/"
+                  target="_blank"
+                  rel="noreferrer noopener"
+                >
+                  <button className="hover:bg-red-600 hover:text-[white] text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg transition ease-in duration-300">
+                    Demo
+                  </button>
+                </a>
+                <a
+                  href="https://github.com/kunalpatil239/MERN-project"
+                  target="_blank"
+                  rel="noreferrer noopener"
+                >
+                  <button className="transition ease-in duration-300 hover:bg-red-600 hover:text-[white] text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div
+            style={{ backgroundImage: `url(${instacart3})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* hover effects */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                Shopping Cart
+              </span>
+              <div className="pt-8 text-center">
+                <a
+                  href="https://instacart-shopping-cart.netlify.app/"
+                  target="_blank"
+                  rel="noreferrer noopener"
+                >
+                  <button className="transition ease-in duration-300 hover:bg-red-600 hover:text-[white] text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a
+                  href="https://github.com/kunalpatil239/Instacart"
+                  target="_blank"
+                  rel="noreferrer noopener"
+                >
+                  <button className="transition ease-in duration-300 hover:bg-red-600 hover:text-[white] text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          {/* grid-item */}
+          <div
+            style={{ backgroundImage: `url(${expense})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* hover effects */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                Expense Tracker
+              </span>
+              <div className="pt-8 text-center">
+                <a
+                  href="https://kunalpatil239.github.io/Expense-Tracker-in-React/"
+                  target="_blank"
+                  rel="noreferrer noopener"
+                >
+                  <button className="transition ease-in duration-300 hover:bg-red-600 hover:text-[white] text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a
+                  href="https://github.com/kunalpatil239/Expense-Tracker-in-React"
+                  target="_blank"
+                  rel="noreferrer noopener"
+                >
+                  <button className="transition ease-in duration-300 hover:bg-red-600 hover:text-[white] text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div
+            style={{ backgroundImage: `url(${todo})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* hover effects */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                Todolist App
+              </span>
+              <div className="pt-8 text-center">
+                <a
+                  href="https://kunalpatil239.github.io/Todo-app/"
+                  target="_blank"
+                  rel="noreferrer noopener"
+                >
+                  <button className="transition ease-in duration-300 hover:bg-red-600 hover:text-[white] text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a
+                  href="https://github.com/kunalpatil239/Todo-app"
+                  target="_blank"
+                  rel="noreferrer noopener"
+                >
+                  <button className="transition ease-in duration-300 hover:bg-red-600 hover:text-[white] text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          {/* grid-item */}
+          <div
+            style={{ backgroundImage: `url(${instafood2})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* hover effects */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                Restaurant Landing Page
+              </span>
+              <div className="pt-8 text-center">
+                <a
+                  href="https://instafood-restaurant.netlify.app/"
+                  target="_blank"
+                  rel="noreferrer noopener"
+                >
+                  <button className="transition ease-in duration-300 hover:bg-red-600 hover:text-[white] text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a
+                  href="https://github.com/kunalpatil239/html-css-restaurant-website"
+                  target="_blank"
+                  rel="noreferrer noopener"
+                >
+                  <button className="transition ease-in duration-300 hover:bg-red-600 hover:text-[white] text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div
+            style={{ backgroundImage: `url(${instafood2})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* hover effects */}
+            <div className="opacity-0 group-hover:opacity-100 ">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                React JS Application
+              </span>
+              <div className="pt-8 text-center">
+                <a
+                  href="https://instafood-restaurant.netlify.app/"
+                  target="_blank"
+                  rel="noreferrer noopener"
+                >
+                  <button className="transition ease-in duration-300 hover:bg-red-600 hover:text-[white] text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a
+                  href="https://github.com/kunalpatil239/html-css-restaurant-website"
+                  target="_blank"
+                  rel="noreferrer noopener"
+                >
+                  <button className="transition ease-in duration-300 hover:bg-red-600 hover:text-[white] text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Work;

f05947361bd6e8cff66f5f171c5dcd1132c3d2ae
Kunal <kunalpa239@gmail.com>
2022-05-04 15:08:46 +0530

    changed work height to auto

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -11,7 +11,7 @@ const Skills = () => {
   return (
     <div
       name="skills"
-      className="pt-[7rem] w-full h-screen bg-[#0a192f] text-gray-300"
+      className="pt-[7rem] w-full h-[110vh] bg-[#0a192f] text-gray-300"
     >
       {/* container */}
       <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">

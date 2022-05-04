b3565b819214097963b681995b0dd0c34432f11d
Kunal <kunalpa239@gmail.com>
2022-05-04 13:31:24 +0530

    changed skills and index css

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -9,7 +9,10 @@ import Mongo from "../assets/mongo.png";
 import GitHub from "../assets/github.png";
 const Skills = () => {
   return (
-    <div name="skills" className="w-full h-screen bg-[#0a192f] text-gray-300">
+    <div
+      name="skills"
+      className="pt-[7rem] w-full h-screen bg-[#0a192f] text-gray-300"
+    >
       {/* container */}
       <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
         <div>
